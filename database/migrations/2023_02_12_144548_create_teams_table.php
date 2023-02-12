5d1908229420fea54c60c5a874ed6fef62f2001d
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2023-02-12 23:22:56 +0800

    add migration tables

--- database/migrations/2023_02_12_144548_create_teams_table.php
+++ database/migrations/2023_02_12_144548_create_teams_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('icon');
+            $table->bigInteger('company_id')->unsigned();
             $table->timestamps();
         });
     }
--- database/migrations/2023_02_12_144557_create_roles_table.php
+++ database/migrations/2023_02_12_144557_create_roles_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->bigInteger('company_id')->unsigned();
             $table->timestamps();
         });
     }
